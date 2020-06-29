d7c8e3c047eb1f08c9c3d3e6fbbd229981a2d57c
ayla-marie <aylawolfgram@gmail.com>
2020-06-29 17:29:24 -0400

    added tokens for private paths

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -18,11 +21,57 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     //res.send(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      //ensures there is no duplicate user accounts
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: "Email registered to existing user" });
+      }
+      //creates a new user
+      user = new User({
+        name,
+        email,
+        password
+      });
+      //salt encrypts the information
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      //saves user data
+      await user.save();
+      //res.send("user saved");
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      //give the user a token to remain logged in
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
     }
   }
 );
