4b63c2a4d662fb6a53ae922355425ecae38de64a
ayla-marie <aylawolfgram@gmail.com>
2020-07-05 23:32:51 -0400

    Added delete contact method, revised and tested get logged in user auth middleware route

--- routes/contacts.js
+++ routes/contacts.js
@@ -98,7 +98,23 @@ router.put("/:id", auth, async (req, res) => {
 //@route    DELETE   api/contacts/:id
 //@desc     Delete contact
 //@access   Private
-router.delete("/:id", (req, res) => {
-  res.send("Delete current contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "contact not found" });
+
+    //make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "contact removed" });
+  } catch (err) {
+    console.error(er.message);
+    res.status(500).send("server error");
+  }
 });
 module.exports = router;
