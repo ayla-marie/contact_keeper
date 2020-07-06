e05338441a64fc91c306412db66547fa283493db
ayla-marie <aylawolfgram@gmail.com>
2020-07-05 23:15:04 -0400

    added and tested update contact function

--- routes/contacts.js
+++ routes/contacts.js
@@ -64,8 +64,35 @@ router.post(
 //@route    PUT    api/contacts/:id
 //@desc     Update contact
 //@access   Private
-router.put("/:id", (req, res) => {
-  res.send("Update current contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "contact not found" });
+
+    //make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.error(er.message);
+    res.status(500).send("server error");
+  }
 });
 
 //@route    DELETE   api/contacts/:id
